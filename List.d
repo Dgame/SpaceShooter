621102192db28841ce486cc518c4a382b0cb21b9
Randy Schütt <rswhite4@gmail.com>
2014-01-16 09:57:22 +0100

    Init

---
+++ List.d
@@ -0,0 +1,155 @@
+module Spaceshooter.List;
+
+final class List(T) {
+	static struct Entry {
+		T value;
+		Entry* next;
+		Entry* previous;
+		
+		alias value this;
+	}
+	
+private:
+	Entry* _top;
+	Entry* _end;
+	size_t _length;
+	
+public:
+	Entry* erase(Entry* it) {
+		if (it is null)
+			return null;
+		
+		Entry* left = it.previous;
+		Entry* right = it.next;
+		
+		if (left !is null)
+			left.next = right;
+		if (right !is null)
+			right.previous = left;
+		
+		if (it is this._top)
+			this._top = right;
+		if (it is this._end)
+			this._end = null;
+		
+		this._length--;
+
+		return right is null ? new Entry(T.init, null, null) : right;
+	}
+	
+	int indexOf(T value) {
+		Entry* it = this._top;
+		for (int i = 0; it.next !is null; i++, it = it.next) {
+			if (it.value == value)
+				return i;
+		}
+		
+		return -1;
+	}
+	
+@safe:
+pure:
+nothrow:
+	void push_back(T value) {
+		Entry* end = this._end;
+		this._end = new Entry(value, null, end);
+		if (end !is null)
+			end.next = this._end;
+		
+		if (this._top is null)
+			this._top = this._end;
+		
+		this._length++;
+	}
+	
+	T pop_back() {
+		Entry* end = this._end;
+		this._end = end.previous;
+		
+		this._length--;
+		
+		return end.value;
+	}
+	
+	void push_front(T value) {
+		Entry* top = this._top;
+		this._top = new Entry(value, top, null);
+		if (top !is null)
+			top.previous = this._top;
+		
+		this._length++;
+	}
+	
+	T pop_front() {
+		Entry* top = this._top;
+		this._top = top.next;
+		
+		this._length--;
+		
+		return top.value;
+	}
+	
+	void insert(size_t index, T value) {
+		if (index == 0)
+			return this.push_front(value);
+		if (index == this._length - 1)
+			return this.push_back(value);
+		
+		Entry* it = this._top;
+		for (size_t i = 0; i < index && it.next !is null; i++, it = it.next) {
+			if (i == index)
+				break;
+		}
+		
+		Entry* left = it.previous;
+		Entry* nit = new Entry(value, it, left);
+		
+		if (left !is null)
+			left.next = nit;
+		it.previous = nit;
+		
+		this._length++;
+	}
+	
+	void erase(size_t index) {
+		if (index == 0) {
+			this.pop_front();
+			
+			return;
+		}
+		
+		if (index == this._length - 1) {
+			this.pop_back();
+			
+			return;
+		}
+		
+		Entry* it = this._top;
+		for (size_t i = 0; i < index && it.next !is null; i++, it = it.next) {
+			if (i == index)
+				break;
+		}
+		
+		Entry* left = it.previous;
+		Entry* right = it.next;
+		
+		if (left !is null)
+			left.next = right;
+		if (right !is null)
+			right.previous = left;
+		
+		this._length--;
+	}
+	
+	inout(Entry)* top() inout {
+		return this._top;
+	}
+	
+	inout(Entry)* end() inout {
+		return this._end;
+	}
+	
+	size_t size() const {
+		return this._length;
+	}
+}
---
+++ main.d
@@ -0,0 +1,146 @@
+import std.stdio;
+
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\derelict\\lib\\dmd\\DerelictSDL2.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\derelict\\lib\\dmd\\DerelictUtil.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\derelict\\lib\\dmd\\DerelictGL3.lib");
+
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameInternal.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameAudio.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameGraphics.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameSystem.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameMath.lib");
+pragma(lib, "E:\\D\\dmd2\\src\\ext\\Dgame\\lib\\Release\\DgameWindow.lib");
+
+import Dgame.Window.all;
+import Dgame.Graphics.all;
+import Dgame.System.all;
+
+import Spaceshooter.List;
+
+enum float Slide = 0.6f;
+enum short Move = 15;
+enum ushort WinWidth = 640;
+enum ushort WinHeight = 480;
+
+void inc_var(ref float d) pure nothrow {
+	if (d < 0f) {
+		d += Slide;
+		if (d > 0f)
+			d = 0f;
+	}
+}
+
+void dec_var(ref float d) pure nothrow {
+	if (d > 0f) {
+		d -= Slide;
+		if (d < 0f)
+			d = 0f;
+	}
+}
+
+void smooth_move(Spritesheet sp, ref float x, ref float y) {
+	if (x != 0) {
+		if (x < 0f)
+			inc_var(x);
+		else
+			dec_var(x);
+	}
+
+	if (y != 0) {
+		if (y < 0f)
+			inc_var(y);
+		else
+			dec_var(y);
+	}
+
+	const float nx = sp.X + x;
+	const float ny = sp.Y + y;
+
+	if ((nx + sp.width) > WinWidth || nx < 0)
+		x = 0f;
+	if ((ny + sp.height) > WinHeight || ny < 0)
+		y = 0f;
+
+	sp.move(x, y);
+}
+
+void main() {
+	Window wnd = new Window(VideoMode(WinWidth, WinHeight), "Dgame Test");
+	wnd.setVerticalSync(Window.Sync.Disable);
+	wnd.setFpsLimit(30);
+
+	Image bullet = new Image("../../images/playerBullet.png");
+
+	Image shooter_img = new Image("../../images/starship_sprite.png");
+	Spritesheet shooter = new Spritesheet(shooter_img, ShortRect(0, 0, 64, 64));
+	shooter.setPosition(150, 50);
+
+	float sx = 0f, sy = 0f;
+
+	List!Sprite bullets = new List!Sprite();
+
+	Event event;
+	while (wnd.isOpen()) {
+		wnd.clear();
+		
+		while (EventHandler.poll(&event)) {
+			switch (event.type) {
+				case Event.Type.Quit:
+					writeln("Quit Event");
+					
+					wnd.close();
+					break;
+
+				case Event.Type.KeyDown:
+					switch (event.keyboard.key) {
+						case Keyboard.Code.Left:
+							sx = -Move;
+							break;
+						case Keyboard.Code.Right:
+							sx = Move;
+							break;
+						case Keyboard.Code.Up:
+							sy = -Move;
+							break;
+						case Keyboard.Code.Down:
+							sy = Move;
+							break;
+						case Keyboard.Code.Space:
+							shooter.row = 1;
+
+							Sprite my_bullet = new Sprite(bullet);
+							my_bullet.setPosition(shooter.X + shooter.width, shooter.Y + (shooter.height / 2));
+							bullets.push_back(my_bullet);
+							break;
+						default: break;
+					}
+					break;
+				
+				case Event.Type.KeyUp:
+					if (event.keyboard.key == Keyboard.Code.Space)
+						shooter.row = 0;
+					break;
+
+				default: break;
+			}
+		}
+
+		smooth_move(shooter, sx, sy);
+		shooter.slideTextureRect(Spritesheet.Grid.Row);
+
+		size_t i = 0;
+		for (auto it = bullets.top(); it !is null; it = it.next) {
+			wnd.draw(it.value);
+
+			it.value.move(Move, 0);
+			if (it.value.X > WinWidth) {
+				writeln("Remove it: ", it.value);
+				it = bullets.erase(it);
+			}
+		}
+
+		wnd.draw(shooter);
+
+		wnd.display();
+	}
+}
